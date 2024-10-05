7e5b6fe63a734a0fa827397c9e16765c1efd464a
brendanmclaughlin <bmclaughlin0407@gmail.com>
2024-10-05 14:22:10 -0700

    build

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="font-work font-normal text-duskwine text-md bg-moonmist">
       <body className=''>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
         </ThemeProvider>
       </body>

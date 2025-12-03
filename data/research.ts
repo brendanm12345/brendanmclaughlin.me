031367ff983b691524229ae299443b19a507cff3
Brendan McLaughlin <72267866+brendanm12345@users.noreply.github.com>
2025-12-03 01:22:54 -0500

    Update research.ts

--- data/research.ts
+++ data/research.ts
@@ -3,7 +3,7 @@ import { link } from "fs"
 export const research: Record<string, any> = {
     weaver: {
         name: 'Weaver: Shrinking the Generation-Verification Gap by Scaling Compute for Verification',
-        description: 'A framework for designing a strong verifier by combining multiple weak, imperfect verifiers.',
+        description: 'A framework that combines multiple weak verifiers (reward models + LM judges) to achieve o3-mini-level accuracy models like Llama 3.3 70B Instruct',
         image: '',
         conferences: ['NeurIPS 2025', 'ES-FoMoIII@ICML2025'],
         links: [
